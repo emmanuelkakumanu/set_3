9f5296edbaa810c3f0761c1a90e943ce775f67d9
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-08-02 20:02:48 +0530

    Create Word_Count.java

---
+++ Word_Count.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Word_Count
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String str;
+		Scanner s=new Scanner(System.in);
+		str=s.nextLine();
+		str=str.trim();
+		int cnt=1;
+		for(int i=0;i<str.length();i++)
+		{
+			
+			if(str.charAt(i)==' ' && str.charAt(i+1)!=' ')
+			cnt++;
+			
+		}
+		System.out.print(cnt);
+	}
+}
