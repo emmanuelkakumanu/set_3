62365232d479bbee31e7168cfbfe4d63defab717
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-08-03 15:31:37 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -10,7 +10,7 @@ class Ideone
 		int n=s.nextInt();
 		int a=1,b=1,next;
 		System.out.print(a+" "+b);
-		for(int i=2;i<=n;i++)
+		for(int i=2;i<n;i++)
 		{
 			next=a+b;
 			System.out.print(" "+next);
